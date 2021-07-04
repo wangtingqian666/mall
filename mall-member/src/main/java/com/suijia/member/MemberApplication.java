cbfbdb89f4b82a0223c84b2242449526bb95ace2
wangtq <1652679961@qq.com>
2021-07-04 14:51:56 +0800

    chushihua

---
+++ mall-member/src/main/java/com/suijia/member/MemberApplication.java
@@ -0,0 +1,17 @@
+package com.suijia.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+/**
+ * @author wangtq
+ * @create 2021/7/4 13:35
+ */
+@SpringBootApplication
+public class MemberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class,args);
+    }
+}
---
+++ mall-ware/src/test/java/com/suijia/ware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suijia.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
